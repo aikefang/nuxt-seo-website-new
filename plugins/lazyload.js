e1801539298f6fe9dfab9ed6a0ad4c6ec6d35d81
zhuangzhaokai <zhaokai.zhuang@gmail.com>
2020-08-04 20:32:59 +0800

    coding

--- plugins/lazyload.js
+++ plugins/lazyload.js
@@ -1,9 +1,5 @@
 import Vue from "vue"
 import VueLazyload from 'vue-lazyload'
-console.log(123, process)
-if (process.browser) {
-  console.log(1111222)
-}
 Vue.use(VueLazyload, {
   // preLoad: 1.77777,
   error: '//static.webascii.cn/webascii/files/404-98vo2le3tzkb.jpg?imageMogr2/auto-orient/strip/format/jpg/interlace/1/quality/80|imageView2/1/w/160/h/90',
